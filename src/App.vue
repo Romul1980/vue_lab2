e73a1291f2dc53e65933a6709c7601ea791e276a
Romul1980 <149105304+Romul1980@users.noreply.github.com>
2023-10-26 21:58:50 +0300

    'Test'

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+
+import Items from "@/Items.vue";
+</script>
+
+
+<template>
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-lg-9 col-xl-7">
+        <div class="card rounded-3">
+          <div class="card-body p-4">
+
+            <h4 class="text-center my-3 pb-3">To Do App</h4>
+
+
+             <Items/>
+
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ </template>
---
+++ src/Items.vue
@@ -0,0 +1,64 @@
+<script setup>
+
+import {ref} from "vue";
+const items = ref([
+  {name: 'Buy groceries for next week', progress: 'In progress'},
+  {name: 'Renew car insurance', progress: 'In progress'},
+  {name: 'Sign up for online course', progress: 'In progress'}
+  ]);
+
+const handleAddTask = () =>{
+
+  let text = document.getElementById("form1").value;
+  if (text != null){
+    if (text == ''){
+       text = 'Something'
+    }
+    items.value.push({name: text, progress: 'In progress'})
+  }
+
+}
+</script>
+<template>
+  <div class="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
+    <div class="col-12">
+      <div class="form-outline">
+        <input type="text" id="form1" class="form-control"/>
+        <label class="form-label" for="form1">Enter a task here</label>
+      </div>
+    </div>
+
+    <div class="col-12">
+      <button type="submit" class="btn btn-primary" @click="handleAddTask()">Save</button>
+    </div>
+
+  </div>
+  <table class="table mb-4">
+    <thead>
+    <tr>
+      <th scope="col">No.</th>
+      <th scope="col">Todo item</th>
+      <th scope="col">Status</th>
+      <th scope="col">Actions</th>
+    </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, id) of items" :key="items">
+        <td> {{id + 1}} </td>
+        <td> {{item.name}} </td>
+        <td> {{item.progress}} </td>
+        <td>
+          <button type="submit" class="btn btn-danger" @click="items.splice(id,1)">Delete</button>
+          <button type="submit" class="btn btn-success ms-1" @click="items[id].progress = 'Finished'">Finished</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+
+</template>
+<script>
+export default {
+  name: 'Items'
+}
+</script>
